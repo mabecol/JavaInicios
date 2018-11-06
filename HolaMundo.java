025bcc3a479e5c1fa315fcabea165c45bbe2eb4d
mabecol <44791772+mabecol@users.noreply.github.com>
2018-11-05 20:52:33 -0500

    primer commit
    mao

---
+++ HolaMundo.java
@@ -0,0 +1,20 @@
+class HolaMundo{
+  //metodo principal de una clase en java
+  // los metodos pueden ser publico privado protegido y por defecto
+  //static hace que el metodo se vuelva un metodo de clase
+  //void quiere decir que no retona nada
+public static void main(String[] args){
+//typedata https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+//byte  8 bit  -128 -->127
+//short 16 bit -32768 -->32767
+//int 32 bit  -2 a la 31 --> 2 a la 31 -1  o unsigned 0 --> 2 a la 32.
+//long 64 bits -2 a 63 -> 2 a 63 -1   unsigned 0 -> 2 a 64
+//float The float data type is a single-precision 32-bit IEEE 754
+//double The double data type is a double-precision 64-bit IEEE 754
+//boolean: The boolean data type has only two possible values: true and false.
+//char: The char data type is a single 16-bit Unicode character. \u0000 -->uffff
+//
+  System.out.println("Hola Mundo me llamo Mauricio y me estoy volviendo un super programador");
+
+}
+}
